c95b2d7388039f3c2b0888af9bc389a8ca1cfa12
Ubuntu <ubuntu@ip-172-31-44-126.eu-west-1.compute.internal>
2021-03-08 11:27:12 +0000

    Deploy app with hello route to cf

---
+++ src/main/java/io/piotal/pal/tracker/WelcomeController.java
@@ -0,0 +1,12 @@
+package io.piotal.pal.tracker;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class WelcomeController {
+    @GetMapping("/")
+    public String sayHello() {
+        return "Hello";
+    }
+}
